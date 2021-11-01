9258231cabd2d437b73202da8999e5c9e6d1358f
suon0001 <56873514+suon0001@users.noreply.github.com>
2021-11-01 13:39:03 +0100

    Initial commit

--- products-details.php
+++ products-details.php
@@ -1,4 +1,13 @@
 <?php
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+include "navigation.php";
+include "./includes/db/connection.php";
+
+$query = "SELECT * FROM product";
+$result = mysqli_query($con, $query)
 ?>
 
 <!DOCTYPE html>
@@ -11,50 +20,49 @@
           class="offer-img">
 </head>
 <body>
+<?php while ($row = $result->fetch_assoc()) { ?>
 
-<?php include "navigation.php" ?>
+    <div class="small-container single-product">
+        <div class="row">
+            <div class="col-2">
+                <img src="includes/images/temperary.jpg" width="100%">
+                <div class="small-img-row">
+                    <div class="small-img-col">
+                        <img src="includes/images/temperary.jpg" alt="">
+                    </div>
+                    <div class="small-img-col">
+                        <img src="includes/images/temperary.jpg" alt="">
+                    </div>
+                    <div class="small-img-col">
+                        <img src="includes/images/temperary.jpg" alt="">
+                    </div>
+                    <div class="small-img-col">
+                        <img src="includes/images/temperary.jpg" alt="">
+                    </div>
+                </div>
 
-<div class="small-container single-product">
-    <div class="row">
-        <div class="col-2">
-            <img src="includes/images/temperary.jpg" width="100%">
-            <div class="small-img-row">
-                <div class="small-img-col">
-                    <img src="includes/images/temperary.jpg" alt="">
-                </div>
-                <div class="small-img-col">
-                    <img src="includes/images/temperary.jpg" alt="">
-                </div>
-                <div class="small-img-col">
-                    <img src="includes/images/temperary.jpg" alt="">
-                </div>
-                <div class="small-img-col">
-                    <img src="includes/images/temperary.jpg" alt="">
-                </div>
+
             </div>
 
+            <div class="col-2">
+                <h1><?php echo $row['title']; ?></h1>
+                <h4><?php echo $row['price']; ?></h4>
+                <select name="" id="color">
+                    <option value="">--- Choose a color ---</option>
+                    <option value="red">Blue</option>
+                    <option value="green">Red</option>
+                    <option value="blue">Green</option>
+                </select>
+                <input type="number" value="1">
+                <input type="button" class="button" value="Add to Cart">
 
-        </div>
-
-        <div class="col-2">
-            <h1>Products name</h1>
-            <h4>price</h4>
-            <select name="" id="color">
-                <option value="">--- Choose a color ---</option>
-                <option value="red">Blue</option>
-                <option value="green">Red</option>
-                <option value="blue">Green</option>
-            </select>
-            <input type="number" value="1">
-            <input type="button" class="button" value="Add to Cart">
-
-            <h3>Product Details</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboret</p>
+                <h3>Product Details</h3>
+                <p><?php echo $row['description']; ?></p>
+            </div>
         </div>
     </div>
-</div>
 
-
+<?php } ?>
 <div class="small-container">
     <div class="row">
         <div class="col-4">
--- products.php
+++ products.php
@@ -7,10 +7,22 @@ include "./includes/db/connection.php";
 
 $productList = $_SERVER['QUERY_STRING'];
 
-$query ="SELECT * FROM 'product' WHERE product_id = $productList"
+$query = "SELECT * FROM product WHERE product_id = $productList";
+$result = mysqli_query($con, $query);
 
 
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Products</title>
+</head>
+<body>
 <div class="row row-2">
     <h2>All Products</h2>
     <select name="" id="categories">
@@ -23,68 +35,36 @@ $query ="SELECT * FROM 'product' WHERE product_id = $productList"
 
 <div class="small-container">
     <div class="row">
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
         <div class="col-4">
             <img src="includes/images/product.jpg">
-            <h4>Product name 1</h4>
+            <h4> <?php echo $row['title']; ?></h4>
             <div class="rating">
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star-o" aria-hidden="true"></i>
-                <i class="fa fa-star-o" aria-hidden="true"></i>
-                <p>price</p>
+                <i class="fa fa-star" aria - hidden="true"></i>
+                <i class="fa fa-star" aria - hidden="true"></i>
+                <i class="fa fa-star" aria - hidden="true"></i>
+                <i class="fa fa-star-o" aria - hidden="true"></i>
+                <i class="fa fa-star-o" aria - hidden="true"></i>
+                <p> price</p>
             </div>
-        </div>
-        <div class="col-4">
-            <img src="includes/images/product.jpg">
-            <h4>Product name 2</h4>
-            <div class="rating">
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star-o" aria-hidden="true"></i>
-                <i class="fa fa-star-o" aria-hidden="true"></i>
-                <p>price</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <img src="includes/images/product.jpg">
-            <h4>Product name 3</h4>
-            <div class="rating">
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star-o" aria-hidden="true"></i>
-                <i class="fa fa-star-o" aria-hidden="true"></i>
-                <p>price</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <img src="includes/images/product.jpg">
-            <h4>Product name 4</h4>
-            <div class="rating">
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star" aria-hidden="true"></i>
-                <i class="fa fa-star-o" aria-hidden="true"></i>
-                <i class="fa fa-star-o" aria-hidden="true"></i>
-                <p>price</p>
-            </div>
-        </div>
-        <div class="page-btn">
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-            <span>&#8594;</span>
         </div>
     </div>
+    <?php } ?>
+    <div class="page-btn">
+        <span> 1</span>
+        <span> 2</span>
+        <span> 3</span>
+        <span> 4</span>
+        <span>&#8594;</span>
+    </div>
+</div>
+
 
 </div>
 
 <?php include("footer.php"); ?>
-
-
 </body>
 </html>
+
+
 
